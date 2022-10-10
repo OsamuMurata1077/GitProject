bba32d5fa5a95ae68fa15642b5a99d5e7014b3d0
murata <murataosa@gmail.com>
2022-10-10 14:18:58 +0900

    新規ブランチで文章追加してコミット

--- GitProject/GitProject.cpp
+++ GitProject/GitProject.cpp
@@ -1,11 +1,10 @@
-﻿// GitProject.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
-//
-
-#include <iostream>
+﻿#include <iostream>
 
 int main()
 {
     std::cout << "Hello GitHub World\n";
+
+    std::cout << "Hello New branch\n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
